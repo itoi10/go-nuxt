052ca6c2c2309bfe38ccd02fc208c4740263f517
itict10 <itict10@gmail.com>
2021-11-25 08:47:01 +0900

    add layouts

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- <TheHeader /> -->
+
+    <div class="column">
+      <!-- <TheSidebar /> -->
+
+      <div class="column content">
+        <nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // Nuxtではlayoutsディレクトリは以下に設置したファイルを画面レイアウトとして利用できる
+
+</script>
+
+<style scoped>
+  /* scoped 現在のコンポーネントの要素にのみ適用  */
+
+  #app {
+    min-height: 100vh;
+    background: #FAFAFA;
+  }
+
+  .columns {
+    min-height: 100vh;
+  }
+</style>
