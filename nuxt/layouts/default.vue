d5f2a66887918645e933b58c16edd87f2f75594f
itict10 <itict10@gmail.com>
2021-11-27 17:30:52 +0900

    add AppVideo component. setup bulma ui

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <TheHeader />
 
-    <div class="column">
+    <div class="columns">
       <TheSidebar />
 
       <div class="column content">
@@ -14,12 +14,20 @@
 
 <script>
   // Nuxtではlayoutsディレクトリは以下に設置したファイルを画面レイアウトとして利用できる
+  import TheHeader from '../components/TheHeader'
+  import TheSidebar from '../components/TheSidebar'
+
+  export default {
+    components: {
+      TheHeader,
+      TheSidebar
+    }
+  }
 
 </script>
 
 <style scoped>
   /* scoped 現在のコンポーネントの要素にのみ適用  */
-
   #app {
     min-height: 100vh;
     background: #FAFAFA;
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,21 +1,53 @@
 <template>
   <sectoin class="section">
     <div class="container">
-
+      <div class="block">
+        <div class="block video-block" v-for="(item, key) in items" :key="key" >
+          <AppVideo
+            :item="item"
+            :video-id="item.id"
+          />
+        </div>
+      </div>
     </div>
   </sectoin>
 </template>
 
-<script lang="ts">
+<script>
 import ROUTES from '..//routes/api'
+import AppVideo from '../components/AppVideo.vue'
 
 export default {
-  async fetch({store}: any) {
+  components: {
+    AppVideo
+  },
+
+  computed: {
+    // getterを使ってストアから動画一覧取得
+    items() {
+      const videos = this.$store.getters.getPopularVideos
+      console.log(videos)
+      return videos
+    }
+  },
+
+  async fetch({store}) {
     const payload = {
       uri: ROUTES.GET.POPULARS
     }
+
+    if (store.getters.getPopularVideos && store.getters.getPopularVideos.length > 0) {
+      return
+    }
+
     // Vuexストアのアクションに処理をdispatch
     await store.dispatch('fetchPopularVideos', payload)
   }
 }
 </script>
+
+<style scoped>
+  .video-block {
+    max-width: 900px;
+  }
+</style>
