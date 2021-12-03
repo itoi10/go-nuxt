184cb0b402bb04b9931db09f27fb0b462502b2af
itict10 <itict10@gmail.com>
2021-12-03 23:05:29 +0900

    add retry process

--- nuxt/store/request-client.js
+++ nuxt/store/request-client.js
@@ -1,9 +1,18 @@
+
+// トークン取得のリクエスト送信時に使用
+import qs from 'qs'
+
+import { firebaseConfig } from '~/plugins/firebaseConfig'
+
 // リクエストを送信するクライアント
 // リクエスト送信にはaxiosを使用するが直接呼び出さずラップする
 
 export class RequestClient {
-  constructor(axios) {
+  constructor(axios, cookies, store) {
     this.axios = axios
+    this.cookies = cookies
+    this.store = store
+    this.hasRetried = false
   }
 
   // GETリクエスト送信で使用するgetメソッド定義
@@ -36,11 +45,43 @@ export class RequestClient {
 
     // リクエスト失敗した時にretry実行
     async retry(err) {
+      const code = parseInt(err.response && err.response.status)
+      // cookieからリフレッシュトークン取得
+      const refreshToken = this.cookies.get('refresh_token') || null
 
+      // 認証失敗 && リフレッシュトークンあり && 1回目のリトライ
+      if (code === 401 && refreshToken && this.hasRetried === false) {
+        this.hasRetried = true
+        if (refreshToken) {
+          const data = {
+            'grant_type': 'refresh_token',
+            'refresh_token': refreshToken
+          }
+          // Firebase Auth REST APIを使用しトークン再取得
+          // https://firebase.google.com/docs/reference/rest/auth#section-api-usage
+          const res = await this.axios.$request({
+            method: 'POST',
+            headers: {'content-type': 'application/x-www-form-urlencoded'},
+            data: qs.stringify(data),
+            url: 'https://securetoken.googleapis.com/v1/token?key=' + firebaseConfig.apiKey
+          })
+          // 取得したトークンを保存
+          this.store.dispatch('setToken', res.id_token)
+
+          // リトライ
+          return await this.axios.$request({
+            method: err.response.config.method,
+            headers: {'Authorization': `Bearer ${res.id_token}`},
+            url: err.response.config.url,
+            data: err.response.config.data
+          })
+        }
+
+      }
     }
 }
 
 // RequestClientインスタンス生成に使う関数
-export function createRequestClient(axios) {
-  return new RequestClient(axios)
+export function createRequestClient(axios, cookies, store) {
+  return new RequestClient(axios, cookies, store)
 }
