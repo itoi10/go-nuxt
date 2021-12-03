2444295dc12e7c6be4ab43cf11c569a3804373b3
itict10 <itict10@gmail.com>
2021-12-03 22:34:16 +0900

    add retry memo

--- nuxt/store/request-client.js
+++ nuxt/store/request-client.js
@@ -13,11 +13,30 @@ export class RequestClient {
     const query = queryString.length > 0 ? `${uri}?${queryString}` : uri
     console.log("query: " + query)
     return await this.axios.$get(query)
+      .catch(err => {
+        return this.retry(err)
+      })
   }
 
     // POST送信
     async post(uri) {
       return await this.axios.$post(uri)
+        .catch(err => {
+          return this.retry(err)
+        })
+    }
+
+    // アクセストークン有効期限は１時間だが、切れた時はトークン再取得を試みるようにする。
+    // 処理の流れは以下となる
+    // 1. (nuxt) goにリクエスト送信
+    // 2. (go)   トークン期限切れの場合401を返す
+    // 3. (nuxt) リフレッシュトークンを使ってトークン再取得
+    // 4. (nuxt) 新しいトークンでAPIリトライ
+    // 5. (go)   新しいトークンを検証し結果返却
+
+    // リクエスト失敗した時にretry実行
+    async retry(err) {
+
     }
 }
 
